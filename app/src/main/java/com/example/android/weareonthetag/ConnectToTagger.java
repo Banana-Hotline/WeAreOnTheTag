4a4ff4a976c285db76487b109ba198b1746fb629
nayrpg <nayrpg@gmail.com>
2015-05-20 21:16:02 -0400

    Bluetooth enabled properly

--- app/src/main/java/com/example/android/weareonthetag/ConnectToTagger.java
+++ app/src/main/java/com/example/android/weareonthetag/ConnectToTagger.java
@@ -1,7 +1,6 @@
 package com.example.android.weareonthetag;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,23 +9,47 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.bluetooth.BluetoothAdapter;
-import android.os.Build;
 import android.widget.TextView;
-
+import android.widget.Button;
+import android.content.Intent;
 
 public class ConnectToTagger extends ActionBarActivity {
+    private static final int REQUEST_ENABLE_BT = 1;
 
+    /** Called when the activity is first created. */
+
+    TextView outputTxtView;
+    BluetoothAdapter bluetoothAdapter;
+    Button connectDevicesBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connect_to_tagger);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+        setContentView(R.layout.fragment_connect_to_tagger);
+
+        outputTxtView = (TextView)findViewById(R.id.outputTxtView);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        connectDevicesBtn = (Button)findViewById(R.id.connectDevicesBtn);
+
+        CheckBlueToothState();
+    }
+    private void CheckBlueToothState(){
+        if (bluetoothAdapter == null){
+            outputTxtView.setText("Bluetooth NOT support");
+        }else{
+            if (bluetoothAdapter.isEnabled()){
+                if(bluetoothAdapter.isDiscovering()){
+                    outputTxtView.setText("Bluetooth is currently in device discovery process.");
+                }else{
+                    outputTxtView.setText("Bluetooth is Enabled.");
+                    connectDevicesBtn.setVisibility(View.VISIBLE);
+                }
+            }else{
+                outputTxtView.setText("Bluetooth is NOT Enabled!");
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,7 +72,19 @@ public class ConnectToTagger extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        if (resultCode == RESULT_OK) {
+            connectDevicesBtn.setVisibility(View.VISIBLE);
+            outputTxtView.setText("Bluetooth is Enabled.");
+        } else if (resultCode == RESULT_CANCELED) {
+            outputTxtView.setText("You must have Bluetooth enabled to connect to game devices.");
+        }
+        else{
+            outputTxtView.setText("There was an issue trying to enable Bluetooth.");
+        }
 
+    }
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -63,18 +98,8 @@ public class ConnectToTagger extends ActionBarActivity {
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_connect_to_tagger, container, false);
             TextView outputTxtView = (TextView)rootView.findViewById(R.id.outputTxtView);
-            BluetoothAdapter btAdp = BluetoothAdapter.getDefaultAdapter();
-            if(btAdp == null)
-            {
-                outputTxtView.setText("Bluetooth not provided on device");
-            }
-            else {
-                if (btAdp.isEnabled() == true) {
-                    outputTxtView.setText("Bluetooth is Enabled");
-                } else {
-                    outputTxtView.setText("Bluetooth is Disabled");
-                }
-            }
+            Button connectDevicesBtn = (Button) rootView.findViewById(R.id.connectDevicesBtn);
+
             return rootView;
         }
     }
