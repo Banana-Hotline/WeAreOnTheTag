965a894879ef2752769ee21642f8daaf063a5ee6
nayrpg <nayrpg@gmail.com>
2015-05-23 14:47:08 -0400

    fixed bluetooth enable

---
+++ app/src/main/java/com/example/android/weareonthetag/ConnectHardwareFragment.java
@@ -0,0 +1,89 @@
+package com.example.android.weareonthetag;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Set;
+
+/**
+ * A fragment containing a view for connecting to tagger hardware.
+ */
+public class ConnectHardwareFragment extends Fragment {
+    BluetoothAdapter bta = MainActivity.bluetoothAdapter;
+    ArrayAdapter mArrayAdapter;
+    View rootView;
+    Button connectDevicesBtn;
+    TextView outputTxtView;
+    BluetoothMessageHandler mListener;
+
+    public ConnectHardwareFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_connect_hardware, container, false);
+        outputTxtView = (TextView) rootView.findViewById(R.id.outputTxtView);
+        connectDevicesBtn = (Button) rootView.findViewById(R.id.connectDevicesBtn);
+        mArrayAdapter = new ArrayAdapter(getActivity(), R.layout.list_item_bt_devices, R.id.list_item_bt_device);
+        connectDevicesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckForKnownDevices();
+                return;
+            }
+        });
+        CheckBlueToothState();
+        return rootView;
+    }
+
+    public boolean CheckForKnownDevices() {
+        Set<BluetoothDevice> pairedDevices = bta.getBondedDevices();
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+        return true;
+    }
+
+    private void CheckBlueToothState() {
+        if (MainActivity.bluetoothAdapter == null) {
+            outputTxtView.setText("Bluetooth NOT supported");
+        } else {
+            if (MainActivity.bluetoothAdapter.isEnabled()) {
+                if (MainActivity.bluetoothAdapter.isDiscovering()) {
+                    outputTxtView.setText("Bluetooth is currently in device discovery process.");
+                } else {
+                    outputTxtView.setText("Bluetooth is Enabled.");
+                    connectDevicesBtn.setVisibility(View.VISIBLE);
+                }
+            } else {
+                outputTxtView.setText("Bluetooth is NOT Enabled!");
+                mListener.onBTDisabled(rootView);
+            }
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+            try {
+                mListener = (BluetoothMessageHandler) activity;
+            } catch (ClassCastException e) {
+                throw new ClassCastException(activity.toString() + " must implement ExampleFragmentCallbackInterface ");
+            }
+    }
+}
--- app/src/main/java/com/example/android/weareonthetag/MainActivity.java
+++ app/src/main/java/com/example/android/weareonthetag/MainActivity.java
@@ -1,25 +1,14 @@
 package com.example.android.weareonthetag;
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.util.Set;
 
 public class MainActivity extends ActionBarActivity implements BluetoothMessageHandler {
     private static final int REQUEST_ENABLE_BT = 1;
@@ -87,80 +76,6 @@ public class MainActivity extends ActionBarActivity implements BluetoothMessageH
         rootView = v;
         Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-    }
-
-    /**
-     * A fragment containing a view for connecting to tagger hardware.
-     */
-    public static class ConnectHardwareFragment extends Fragment {
-        BluetoothAdapter bta = MainActivity.bluetoothAdapter;
-        ArrayAdapter mArrayAdapter;
-        View rootView;
-        Button connectDevicesBtn;
-        TextView outputTxtView;
-        BluetoothMessageHandler mListener;
-
-        public ConnectHardwareFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            rootView = inflater.inflate(R.layout.fragment_connect_hardware, container, false);
-            outputTxtView = (TextView) rootView.findViewById(R.id.outputTxtView);
-            connectDevicesBtn = (Button) rootView.findViewById(R.id.connectDevicesBtn);
-            mArrayAdapter = new ArrayAdapter(getActivity(), R.layout.list_item_bt_devices, R.id.list_item_bt_device);
-            connectDevicesBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    CheckForKnownDevices();
-                    return;
-                }
-            });
-            CheckBlueToothState();
-            return rootView;
-        }
-
-        public boolean CheckForKnownDevices() {
-            Set<BluetoothDevice> pairedDevices = bta.getBondedDevices();
-            // If there are paired devices
-            if (pairedDevices.size() > 0) {
-                // Loop through paired devices
-                for (BluetoothDevice device : pairedDevices) {
-                    // Add the name and address to an array adapter to show in a ListView
-                    mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-                }
-            }
-            return true;
-        }
-
-        private void CheckBlueToothState() {
-            if (bluetoothAdapter == null) {
-                outputTxtView.setText("Bluetooth NOT supported");
-            } else {
-                if (bluetoothAdapter.isEnabled()) {
-                    if (bluetoothAdapter.isDiscovering()) {
-                        outputTxtView.setText("Bluetooth is currently in device discovery process.");
-                    } else {
-                        outputTxtView.setText("Bluetooth is Enabled.");
-                        connectDevicesBtn.setVisibility(View.VISIBLE);
-                    }
-                } else {
-                    outputTxtView.setText("Bluetooth is NOT Enabled!");
-                    mListener.onBTDisabled(rootView);
-                }
-            }
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-                try {
-                    mListener = (BluetoothMessageHandler) activity;
-                } catch (ClassCastException e) {
-                    throw new ClassCastException(activity.toString() + " must implement ExampleFragmentCallbackInterface ");
-                }
-        }
     }
 
 }
